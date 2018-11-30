6045bdd855e61b2d3e41c9f7945b982678f56292
（´∀｀） XTL <395038209@qq.com>
2018-12-01 02:07:27 +0800

    update code

--- app/js/home/index.vue
+++ app/js/home/index.vue
@@ -1,18 +1,17 @@
 <template lang="html">
-    <div :class="$style.home">
-        <h1>Home</h1>
-    </div>
+  <div>
+    <h1>Home</h1>
+  </div>
 </template>
 
 <script>
-export default {
-}
+export default {};
 </script>
 
 <style lang="scss" module>
-  @import '../../css/reset.scss';
-  .home{
-    color: red;
-    font-size: 80px;
-  }
+@import "../../css/reset.scss";
+.home {
+  color: red;
+  font-size: 80px;
+}
 </style>
--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,9 @@
 import Vue from "vue"
+import VueAwesomeSwiper from "vue-awesome-swiper"
 import App from "./App.vue"
 import router from "./router"
+
+Vue.use(VueAwesomeSwiper)
 
 Vue.config.productionTip = false
 
